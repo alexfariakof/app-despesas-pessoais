ebbc9de58b215e882dd3171cd175e8dd635102a9
Ribeiro <42475620+alexfariakof@users.noreply.github.com>
2023-05-13 18:10:39 -0300

    Add Usuario  Controller

---
+++ Controllers/UsuarioController.cs
@@ -0,0 +1,67 @@
+﻿using despesas_backend_api_net_core.Business.Generic;
+using despesas_backend_api_net_core.Domain.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace despesas_backend_api_net_core.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : Controller
+    {
+        private IBusiness<Usuario> _usuarioBusiness;
+
+        public UsuarioController(IBusiness<Usuario> usuarioBusiness)
+        {
+            _usuarioBusiness = usuarioBusiness;            
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+           return Ok(_usuarioBusiness.FindAll());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            Usuario _usuario = _usuarioBusiness.FindById(id);
+
+            if (_usuario == null)
+                return NotFound();
+
+            return Ok(_usuario);
+        }
+
+        [HttpPost]
+        //[Authorize("Bearer")]
+        public IActionResult Post([FromBody] Usuario usuario)
+        {
+            if (usuario == null)
+                return BadRequest();
+            return new ObjectResult(_usuarioBusiness.Create(usuario));
+        }
+
+        [HttpPut]
+        //[Authorize("Bearer")]
+        public IActionResult Put([FromBody] Usuario usuario)
+        {
+            if (usuario == null)
+                return BadRequest();
+
+            Usuario updateUsuario = _usuarioBusiness.Update(usuario);
+            if (updateUsuario == null)
+                return NoContent();
+
+            return new ObjectResult(updateUsuario);
+        }
+
+        [HttpDelete("{id}")]
+        //[Authorize("Bearer")]
+        public IActionResult Delete(int id)
+        {
+            _usuarioBusiness.Delete(id);
+            return NoContent();
+        }
+    }
+}
--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,9 @@
 #See https://aka.ms/customizecontainer to learn how to customize your debug container and how Visual Studio uses this Dockerfile to build your images for faster debugging.
-FROM ubuntu:22.04 AS builder
-
-# install the .NET 6 SDK from the Ubuntu archive
-# (no need to clean the apt cache as this is an unpublished stage)
-RUN apt-get update && apt-get install -y dotnet6 ca-certificates
 
 FROM mcr.microsoft.com/dotnet/aspnet:6.0 AS base
 WORKDIR /app
-EXPOSE 5256
-EXPOSE 5257
+EXPOSE 80
+EXPOSE 443
 
 FROM mcr.microsoft.com/dotnet/sdk:6.0 AS build
 WORKDIR /src
