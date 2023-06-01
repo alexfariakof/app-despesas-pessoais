d06109bc00e8f9bcba21b58db36fba8ff15c6daa
Ribeiro <42475620+alexfariakof@users.noreply.github.com>
2023-06-01 19:59:48 -0300

    Implementação AmzonS3Bucket to save ImagePerfil

---
+++ Controllers/PerfilFileController.cs
@@ -0,0 +1,107 @@
+﻿using despesas_backend_api_net_core.Business.Generic;
+using despesas_backend_api_net_core.Domain.VM;
+using despesas_backend_api_net_core.Infrastructure.ExtensionMethods;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+
+namespace despesas_backend_api_net_core.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PerfilFileController : ControllerBase
+    {
+        private IBusiness<PerfilUsuarioFileVM> _perfilFileBusiness;
+        public PerfilFileController(IBusiness<PerfilUsuarioFileVM> perfilFileBusiness)
+        {
+            _perfilFileBusiness = perfilFileBusiness;
+        }
+
+        [HttpPost("GetByIdUsuario")]
+        //[Authorize("Bearer")]
+        public IActionResult GetByIdUsuario([FromBody]int idUsuario)
+        {
+            var perfilFile = _perfilFileBusiness.FindAll()
+                .FindAll(prop => prop.UsuarioId.Equals(idUsuario));
+            
+            if (perfilFile != null)  
+                return Ok(perfilFile);
+
+            return BadRequest("Arquivo Inexistente!");
+        }
+
+
+        [HttpPost("Upload")]
+        public async Task<IActionResult> Upload(int idUsuario, IFormFile file)
+        {
+            try
+            {
+                string fileName = "perfil-usuario-" + idUsuario + "-" + DateTime.Now.ToString("yyyyMMddMMHHmmssffffff");
+                string typeFile = "";
+                int posicaoUltimoPontoNoArquivo = file.FileName.LastIndexOf('.');
+                if (posicaoUltimoPontoNoArquivo >= 0 && posicaoUltimoPontoNoArquivo < file.FileName.Length - 1)
+                {
+                    typeFile = file.FileName.Substring(posicaoUltimoPontoNoArquivo + 1);
+                }
+
+                    using (var memoryStream = new MemoryStream())
+                {
+
+                    await file.CopyToAsync(memoryStream);
+                    
+                    PerfilUsuarioFileVM perfilUsuarioFile = new PerfilUsuarioFileVM
+                    {
+                        Arquivo = memoryStream.GetBuffer(),
+                        UsuarioId = idUsuario,
+                        Name = fileName,
+                        Type = typeFile,
+                        ContentType = file.ContentType
+                    };
+
+                    perfilUsuarioFile =  _perfilFileBusiness.Create(perfilUsuarioFile);
+                    return Ok(perfilUsuarioFile);
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { ex });
+            }
+        }
+
+
+        [HttpPost("UploadFiles")]
+        public async Task<IActionResult> Upload(int idUsuario, int IdTipoLancamento, List<IFormFile> files)
+        {
+
+            try
+            {
+                long size = files.Sum(f => f.Length);
+
+                // full path to file in temp location
+                //var filePath = Path.GetTempFileName();
+                var filePath = Path.Combine(Directory.GetCurrentDirectory(), "FilesDownload");
+
+                foreach (var formFile in files)
+                {
+                    if (formFile.Length > 0)
+                    {
+                        using (var stream = new FileStream(filePath, FileMode.Create))
+                        {
+                            await formFile.CopyToAsync(stream);
+                        }
+                    }
+                }
+
+                // process uploaded files
+                // Don't rely on or trust the FileName property without validation.
+
+                return Ok(new { count = files.Count, size, filePath });
+            }
+            catch
+            {
+                return BadRequest();
+            }            
+        }
+ 
+    }
+}
---
+++ Infrastructure/ExtensionMethods/AmazonS3Bucket.cs
@@ -0,0 +1,61 @@
+﻿using Amazon;
+using Amazon.S3;
+using Amazon.S3.Model;
+using despesas_backend_api_net_core.Domain.VM;
+using Newtonsoft.Json;
+
+namespace despesas_backend_api_net_core.Infrastructure.ExtensionMethods
+{
+    public class AmazonS3Bucket
+    {
+        private static readonly S3CannedACL fileCannedACL = S3CannedACL.PublicRead;
+        private static readonly RegionEndpoint bucketRegion = RegionEndpoint.SAEast1;
+        private static IAmazonS3 client;
+        
+        public  class AmazomKeys
+        {
+            public string AccessKey { get; set; }
+            public string SecretAccessKey { get; set; }
+            public string S3ServiceUrl { get; set; }
+        }
+
+        public static async Task<string> WritingAnObjectAsync(PerfilUsuarioFileVM perfilFile)
+        {
+            string arquivoJson = "AMZOM_KEYS.json";
+            try
+            {
+                string json = File.ReadAllText(arquivoJson);
+
+
+                var amazomKeys = JsonConvert.DeserializeObject <AmazomKeys>(json);
+                string bucketName = "bucket-usuario-perfil";
+                string fileContentType = perfilFile.ContentType;
+                AmazonS3Config config = new AmazonS3Config();
+                config.ServiceURL = amazomKeys.S3ServiceUrl;
+
+                client = new AmazonS3Client(amazomKeys.AccessKey, amazomKeys.SecretAccessKey, config);
+    
+                var putRquest = new PutObjectRequest
+                {
+                    CannedACL = fileCannedACL,
+                    BucketName = bucketName,
+                    Key = perfilFile.Name,
+                    ContentType = perfilFile.ContentType,
+                    InputStream = new System.IO.MemoryStream(perfilFile.Arquivo)
+                };
+                PutObjectResponse response = await client.PutObjectAsync(putRquest);
+                var url = $"https://{bucketName}.s3.amazonaws.com/{perfilFile.Name}";
+                return url;
+
+            }
+            catch (AmazonS3Exception ex)
+            {
+                throw new Exception("Error encountered ***. Message:'{0}' when writing an object", ex);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
