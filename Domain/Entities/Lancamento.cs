81207d8f13570268dceb134e78166ef131f82422
Ribeiro <42475620+alexfariakof@users.noreply.github.com>
2023-06-05 14:51:13 -0300

    Adicionando Campo data de Criação

--- Domain/Entities/Lancamento.cs
+++ Domain/Entities/Lancamento.cs
@@ -13,5 +13,6 @@
         public virtual Receita? Receita { get; set; }
         public int CategoriaId { get; set; }
         public virtual Categoria? Categoria { get; set; }
+        public DateTime? DataCriacao { get; set; }
     }
 }
