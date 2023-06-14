6f57eecfe76ef870f692cb62932fd455359866e1
Ribeiro <42475620+alexfariakof@users.noreply.github.com>
2023-06-14 19:31:18 -0300

    Correção na criação da Procedure campo DataCriacao retoma now() ao invés de null
    Este problema resolve erro no EntityFrameWork que espera receber um valor diferente de null

--- .documents/create-procedure-lancamento-por-mes-ano.sql
+++ .documents/create-procedure-lancamento-por-mes-ano.sql
@@ -4,14 +4,14 @@ CREATE PROCEDURE `LancamentosPorMesAno`(IN IdUsuario INT,IN mes INT, IN ano INT)
 BEGIN
 
 Select cast(CONV(SUBSTRING(uuid(), 4, 4), 16, 10) as UNSIGNED) as id, lancamentos.* From (  
-Select d.UsuarioId, data, d.CategoriaId, valor as valor, d.id as DespesaId, 0 as ReceitaId, d.descricao, null as DataCriacao  
+Select d.UsuarioId, data, d.CategoriaId, valor as valor, d.id as DespesaId, 0 as ReceitaId, d.descricao, now() as DataCriacao  
   FROM Despesa d  
  Inner Join Categoria c on d.CategoriaId = c.id  
  where d.UsuarioId = IdUsuario  
    and Month(data) = mes  
    and  Year(data) = ano  
  union  
-Select r.UsuarioId, data, r.CategoriaId, valor,  0 as DespesaId, r.id as ReceitaId, r.descricao,  null as DataCriacao  
+Select r.UsuarioId, data, r.CategoriaId, valor,  0 as DespesaId, r.id as ReceitaId, r.descricao,  now() as DataCriacao  
   FROM Receita r  
  Inner Join Categoria cr on r.CategoriaId = cr.id  
  where r.UsuarioId = IdUsuario  
