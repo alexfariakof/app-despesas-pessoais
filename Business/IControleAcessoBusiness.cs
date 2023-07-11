32b2fe9b9130e39ae48f23c7437152375440689a
Ribeiro <42475620+alexfariakof@users.noreply.github.com>
2023-07-10 21:10:31 -0300

    Mudança no retorno do método FindByLogin para AuthResponse

--- Business/IControleAcessoBusiness.cs
+++ Business/IControleAcessoBusiness.cs
@@ -5,9 +5,8 @@ namespace despesas_backend_api_net_core.Business
 {
     public interface IControleAcessoBusiness
     {
-        object FindByLogin(ControleAcesso controleAcesso);
+        AuthResponse FindByLogin(ControleAcesso controleAcesso);
         bool Create(ControleAcesso controleAcesso);
-
         bool ChangePassword(int idUsuario, string password);
         bool RecoveryPassword(string email);     
     }
