b4fd7b0105430ad5088ac74e60feab2b32a629e4
Ribeiro <42475620+alexfariakof@users.noreply.github.com>
2023-10-06 21:19:30 -0300

    Implementation Test Unit ControleAcesso Entity

---
+++ despesas-backend-api-net-core.XUnit/Domain/Entities/ControleAcessoTest.cs
@@ -0,0 +1,32 @@
+﻿namespace Test.XUnit.Domain.Entities
+{
+    public class ControleAcessoTest
+    {
+        [Theory]
+        [InlineData(1, "Teste 1", "Teste password 1 ", 1 , typeof(Usuario))]
+        [InlineData(1, "Teste 2", "Teste password 2 ", 2, typeof(Usuario))]
+        [InlineData(3, "Teste 3", "Teste password 3 ", 3, typeof(Usuario))]
+        public void ControleAcesso_ShouldSetPropertiesCorrectly(int id, string login, string senha, int usuarioId, Type type)
+        {
+            var mockUsuario = Mock.Of<Usuario>();
+
+            // Arrange and Act
+            var controleAcesso = new ControleAcesso
+            { 
+                Id = id,
+                Login = login, 
+                Senha = senha,
+                UsuarioId = usuarioId,
+                Usuario  = mockUsuario              
+                
+            };
+            
+            // Assert
+            Assert.Equal(id, controleAcesso.Id);
+            Assert.Equal(login, controleAcesso.Login);
+            Assert.Equal(senha, controleAcesso.Senha);
+            Assert.Equal(usuarioId, controleAcesso.UsuarioId);
+            Assert.Equal(mockUsuario, controleAcesso.Usuario);
+        }
+    }
+}
