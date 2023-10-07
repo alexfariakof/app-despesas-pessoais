40e39b78371d61779b23bd8b2d523b6c8b2733a4
Ribeiro <42475620+alexfariakof@users.noreply.github.com>
2023-10-06 21:53:20 -0300

    Implementation Test Unit Receita Entity

---
+++ despesas-backend-api-net-core.XUnit/Domain/Entities/ReceitaTest.cs
@@ -0,0 +1,41 @@
+﻿namespace Test.XUnit.Domain.Entities
+{
+    public class ReceitaTest
+    {
+        [Theory]
+        [InlineData(1, "Descrição 1", 10.0, 1, typeof(Usuario), 1, typeof(Categoria))]
+        [InlineData(2, "Descrição 2", 0.0, 2, typeof(Usuario), 2, typeof(Categoria))]
+        [InlineData(3, "Descrição 3", 9.5, 3, typeof(Usuario), 3, typeof(Categoria))]
+        public void Receita_ShouldSetPropertiesCorrectly(int id, string descricao, Decimal valor, int usuarioId, Type typeUsuario, int categoriaId, Type typeCategoria )
+        {
+            var mockUsuario = Mock.Of<Usuario>();
+            var mockCategoria= Mock.Of<Categoria>();
+            DateTime data = DateTime.Now;
+
+
+            // Arrange and Act
+            var receita = new Receita
+            { 
+                Id = id,
+                Data = data,
+                Descricao = descricao,
+                Valor = valor,            
+                UsuarioId = usuarioId,                
+                Usuario  = mockUsuario,
+                CategoriaId = categoriaId,
+                Categoria = mockCategoria,
+                
+            };
+            
+            // Assert
+            Assert.Equal(id, receita.Id);
+            Assert.Equal(data, receita.Data);
+            Assert.Equal(descricao, receita.Descricao);
+            Assert.Equal(valor, receita.Valor);
+            Assert.Equal(usuarioId, receita.UsuarioId);
+            Assert.Equal(mockUsuario, receita.Usuario);
+            Assert.Equal(categoriaId, receita.CategoriaId);
+            Assert.Equal(mockCategoria, receita.Categoria);
+        }
+    }
+}
