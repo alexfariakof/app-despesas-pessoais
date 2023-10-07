349ff325e5b6d95727d9f23667a0a8bfbeb6b0d6
Ribeiro <42475620+alexfariakof@users.noreply.github.com>
2023-10-07 03:53:39 -0300

    Fixing da branch dev with  feature/test-xunit-implmentation

---
+++ despesas-backend-api-net-core.XUnit/Business/Generic/GenericBusinessTest.cs
@@ -0,0 +1,108 @@
+﻿using despesas_backend_api_net_core.Business.Generic;
+using despesas_backend_api_net_core.Domain.Entities;
+using despesas_backend_api_net_core.Infrastructure.Data.Repositories.Generic;
+using Moq;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Test.XUnit.Business.Generic
+{
+    public class GenericBusinessTests
+    {
+        [Fact]
+        public void Create_ShouldReturnInsertedObject()
+        {
+            // Arrange
+            var obj = new BaseModel();
+            var repositoryMock = new Mock<IRepositorio<BaseModel>>();
+            repositoryMock.Setup(repo => repo.Insert(obj)).Returns(obj);
+            var business = new GenericBusiness<BaseModel>(repositoryMock.Object);
+
+            // Act
+            var result = business.Create(obj);
+
+            // Assert
+            Assert.Equal(obj, result);
+        }
+
+        [Fact]
+        public void FindAll_ShouldReturnAllObjects()
+        {
+            // Arrange
+            var objects = new List<BaseModel> { new BaseModel(), new BaseModel() };
+            var repositoryMock = new Mock<IRepositorio<BaseModel>>();
+            repositoryMock.Setup(repo => repo.GetAll()).Returns(objects);
+            var business = new GenericBusiness<BaseModel>(repositoryMock.Object);
+
+            // Act
+            var result = business.FindAll(1);
+
+            // Assert
+            Assert.Equal(objects, result);
+        }
+
+        [Fact]
+        public void FindById_ShouldReturnObjectWithMatchingId()
+        {
+            // Arrange
+            var id = 1;
+            var obj = new BaseModel { Id = id };
+            var repositoryMock = new Mock<IRepositorio<BaseModel>>();
+            repositoryMock.Setup(repo => repo.Get(id)).Returns(obj);
+            var business = new GenericBusiness<BaseModel>(repositoryMock.Object);
+
+            // Act
+            var result = business.FindById(id, 1);
+
+            // Assert
+            Assert.Equal(obj, result);
+        }
+
+        [Fact]
+        public void Update_ShouldReturnUpdatedObject()
+        {
+            // Arrange
+            var obj = new BaseModel();
+            var repositoryMock = new Mock<IRepositorio<BaseModel>>();
+            repositoryMock.Setup(repo => repo.Update(obj)).Returns(obj);
+            var business = new GenericBusiness<BaseModel>(repositoryMock.Object);
+
+            // Act
+            var result = business.Update(obj);
+
+            // Assert
+            Assert.Equal(obj, result);
+        }
+
+        [Fact]
+        public void Delete_ShouldReturnTrueIfDeletedSuccessfully()
+        {
+            // Arrange
+            var id = 1;
+            var repositoryMock = new Mock<IRepositorio<BaseModel>>();
+            repositoryMock.Setup(repo => repo.Delete(id)).Returns(true);
+            var business = new GenericBusiness<BaseModel>(repositoryMock.Object);
+
+            // Act
+            var result = business.Delete(id);
+
+            // Assert
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void FindByIdUsuario_ShouldReturnEmptyList()
+        {
+            // Arrange
+            var idUsuario = 1;
+            var business = new GenericBusiness<BaseModel>(Mock.Of<IRepositorio<BaseModel>>());
+
+            // Act
+            var result = business.FindByIdUsuario(idUsuario);
+
+            // Assert
+            Assert.Empty(result);
+        }
+    }
+}
+
---
+++ despesas-backend-api-net-core.XUnit/Infrastructure/Data/IParser.cs
@@ -0,0 +1,8 @@
+﻿namespace Test.XUnit.Infrastructure.Data
+{
+    public interface IParser<O, D>
+    {
+        D Parse(O origin);
+        List<D> ParseList(List<O> origin);
+    }
+}
