a2d9a3bfe2cdeaec4a2cf13814a8eac3fb65a047
Ribeiro <42475620+alexfariakof@users.noreply.github.com>
2023-10-06 21:59:40 -0300

    Implementation Test Unit ImagemPerfilUsuario Entity

---
+++ despesas-backend-api-net-core.XUnit/Domain/Entities/ImagemPerfilUsuarioTest.cs
@@ -0,0 +1,33 @@
+﻿namespace Test.XUnit.Domain.Entities
+{
+    public class ImagemPerfilUsuarioTest
+    {
+        [Theory]
+        [InlineData(1, "http://localhost/usuario1", "usuario 1", "jpg", 1, typeof(Usuario))]
+        [InlineData(2, "http://localhost/usuario2", "usuario 2", "png", 2, typeof(Usuario))]
+        [InlineData(3, "http://localhost/usuario3", "usuario 3", "jpeg", 3, typeof(Usuario))]
+        public void ImagemPerfilUsuario_ShouldSetPropertiesCorrectly(int id, string url, string name, string type, int usuarioId, Type typeUsuario )
+        {
+            var mockUsuario = Mock.Of<Usuario>();
+
+            // Arrange and Act
+            var imagemPerfilUsuario = new ImagemPerfilUsuario
+            { 
+                Id = id,
+                Url = url,
+                Name = name,
+                Type = type,
+                UsuarioId = usuarioId,                
+                Usuario  = mockUsuario                
+            };
+            
+            // Assert
+            Assert.Equal(id, imagemPerfilUsuario.Id);
+            Assert.Equal(url, imagemPerfilUsuario.Url);
+            Assert.Equal(name, imagemPerfilUsuario.Name);
+            Assert.Equal(type, imagemPerfilUsuario.Type);
+            Assert.Equal(usuarioId, imagemPerfilUsuario.UsuarioId);
+            Assert.Equal(mockUsuario, imagemPerfilUsuario.Usuario);
+        }
+    }
+}
