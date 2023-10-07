c8a949ae748ec1e1788a13026b7511059f0ac590
Ribeiro <42475620+alexfariakof@users.noreply.github.com>
2023-10-07 20:01:17 -0300

    Maked Implementatiom of Test Unit Grafico Entity

--- despesas-backend-api-net-core.XUnit/Domain/Entities/GraficoTest.cs
+++ despesas-backend-api-net-core.XUnit/Domain/Entities/GraficoTest.cs
@@ -1,11 +1,24 @@
-﻿namespace Test.XUnit.Domain.Entities
+﻿using System.Collections.Generic;
+
+namespace Test.XUnit.Domain.Entities
 {
     public class GraficoTest
     {
         [Fact]
         public void GraficoTest_ShouldSetPropertiesCorrectly()
         {
-            Assert.Equal(true, true);
+            var somatorioDespesasPorAno = new Mock<Dictionary<String, decimal>>().Object;
+
+            var somatorioReceitasPorAno =  new Mock<Dictionary<String, decimal>>().Object;
+
+            var grafico = new Grafico
+            {
+                SomatorioDespesasPorAno = somatorioDespesasPorAno,
+                SomatorioReceitasPorAno = somatorioReceitasPorAno
+            };
+
+            Assert.Equal(grafico.SomatorioDespesasPorAno, somatorioDespesasPorAno);
+            Assert.Equal(grafico.SomatorioReceitasPorAno, somatorioReceitasPorAno);            
         }
     }
 }
