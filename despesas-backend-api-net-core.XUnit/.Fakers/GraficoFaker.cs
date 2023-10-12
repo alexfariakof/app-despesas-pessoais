b55c4e08b09db520dfe57c7bb8acfd433180834e
Ribeiro <42475620+alexfariakof@users.noreply.github.com>
2023-10-12 02:23:39 -0300

    'Merge tag feature/test-unit-lancamento-controller'

--- despesas-backend-api-net-core.XUnit/.Fakers/GraficoFaker.cs
+++ despesas-backend-api-net-core.XUnit/.Fakers/GraficoFaker.cs
@@ -9,16 +9,35 @@ namespace despesas_backend_api_net_core.XUnit.Fakers
             return new Faker<Grafico>()
             .RuleFor(g => g.SomatorioDespesasPorAno, f => new Dictionary<string, decimal>
             {
-                { "2020", f.Random.Decimal(1000, 5000) },
-                { "2021", f.Random.Decimal(1000, 5000) },
-                { "2022", f.Random.Decimal(1000, 5000) },
+                { "Janeiro", f.Random.Decimal(1000, 5000) },
+                { "Fevereiro", f.Random.Decimal(1000, 5000) },
+                { "Março", f.Random.Decimal(1000, 5000) },
+                { "Abril", f.Random.Decimal(1000, 5000) },
+                { "Maio", f.Random.Decimal(1000, 5000) },
+                { "Junho", f.Random.Decimal(1000, 5000) },
+                { "Julho", f.Random.Decimal(1000, 5000) },
+                { "Agosto", f.Random.Decimal(1000, 5000) },
+                { "Setembro", f.Random.Decimal(1000, 5000) },
+                { "Outubro", f.Random.Decimal(1000, 5000) },
+                { "Novembro", f.Random.Decimal(1000, 5000) },
+                { "Dezembro", f.Random.Decimal(1000, 5000) },
             })
             .RuleFor(g => g.SomatorioReceitasPorAno, f => new Dictionary<string, decimal>
             {
-                { "2020", f.Random.Decimal(1000, 5000) },
-                { "2021", f.Random.Decimal(1000, 5000) },
-                { "2022", f.Random.Decimal(1000, 5000) },
+                { "Janeiro", f.Random.Decimal(1000, 5000) },
+                { "Fevereiro", f.Random.Decimal(1000, 5000) },
+                { "Março", f.Random.Decimal(1000, 5000) },
+                { "Abril", f.Random.Decimal(1000, 5000) },
+                { "Maio", f.Random.Decimal(1000, 5000) },
+                { "Junho", f.Random.Decimal(1000, 5000) },
+                { "Julho", f.Random.Decimal(1000, 5000) },
+                { "Agosto", f.Random.Decimal(1000, 5000) },
+                { "Setembro", f.Random.Decimal(1000, 5000) },
+                { "Outubro", f.Random.Decimal(1000, 5000) },
+                { "Novembro", f.Random.Decimal(1000, 5000) },
+                { "Dezembro", f.Random.Decimal(1000, 5000) },
             });
+
         }
     }
 }
