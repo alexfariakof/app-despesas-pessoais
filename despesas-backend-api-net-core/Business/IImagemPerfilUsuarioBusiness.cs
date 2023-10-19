e503c53e395cda823f531c46554aea48a8cbe65a
Alex Ribeiro de Faria <42475620+alexfariakof@users.noreply.github.com>
2023-10-18 23:08:05 -0300

    Feature/test xunit implementation with 95.3% Coverage Test in Sonar Cloud (#95)
    * 'Merge tag test-unit-implmentations'
    
    * 'Merge tag Implementação de Teste Unitário para cobertura de código da classe ImagemPerfilUsuarioController'

---
+++ despesas-backend-api-net-core/Business/IImagemPerfilUsuarioBusiness.cs
@@ -0,0 +1,15 @@
+﻿using despesas_backend_api_net_core.Domain.Entities;
+using despesas_backend_api_net_core.Domain.VM;
+
+namespace despesas_backend_api_net_core.Business
+{
+    public interface IImagemPerfilUsuarioBusiness
+    {
+        ImagemPerfilUsuarioVM Create(ImagemPerfilUsuarioVM obj);
+        ImagemPerfilUsuarioVM FindById(int id, int idUsuario);
+        List<ImagemPerfilUsuarioVM> FindAll(int idUsuario);
+        ImagemPerfilUsuarioVM Update(ImagemPerfilUsuarioVM obj);
+        bool Delete(ImagemPerfilUsuarioVM obj);
+        UsuarioVM FindByIdUsuario(int idUsuario);
+    }
+}
