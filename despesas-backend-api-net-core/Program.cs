e559323bd1fb7a85b0570b55d83a23844eddd58b
Ribeiro <42475620+alexfariakof@users.noreply.github.com>
2023-10-21 08:19:03 -0300

    Implementação de Teste Unitário da Classe AmazonS3Bucket

--- despesas-backend-api-net-core/Program.cs
+++ despesas-backend-api-net-core/Program.cs
@@ -41,7 +41,6 @@ options.UseMySQL(builder.Configuration.GetConnectionString("MySqlConnectionStrin
 
 ConfigureAutorization(builder.Services, builder.Configuration);
 ConfigureCrypto(builder.Services, builder.Configuration);
-ConfigureAmazonS3Access(builder.Services, builder.Configuration);
 builder.Services.AddRepositories();
 builder.Services.AddServices();
 
@@ -128,12 +127,4 @@ static void ConfigureAutorization(IServiceCollection services, IConfiguration co
 static void ConfigureCrypto(IServiceCollection services, IConfiguration configuration)
 {
     Crypto.GetInstance.SetCryptoKey(configuration.GetSection("Crypto:Key").Value);
-}
-static void ConfigureAmazonS3Access(IServiceCollection services, IConfiguration configuration)
-{
-    var accessKey = configuration.GetSection("AmazonS3Bucket:accessKey").Value;
-    var secretAccessKey = configuration.GetSection("AmazonS3Bucket:secretAccessKey").Value;
-    var s3ServiceUrl = configuration.GetSection("AmazonS3Bucket:s3ServiceUrl").Value;
-    var bucketName = configuration.GetSection("AmazonS3Bucket:bucketName").Value;
-    AmazonS3Bucket.GetInstance.SetConfiguration(accessKey, secretAccessKey, s3ServiceUrl, bucketName);
 }
