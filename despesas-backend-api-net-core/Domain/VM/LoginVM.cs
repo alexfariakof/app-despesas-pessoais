23694e932b1df6db5e294e8bff662e23c8f3f9fb
Ribeiro <alexfariakof@gmail.com>
2023-11-27 14:59:29 -0300

    Update LoginVM.cs

--- despesas-backend-api-net-core/Domain/VM/LoginVM.cs
+++ despesas-backend-api-net-core/Domain/VM/LoginVM.cs
@@ -1,7 +1,7 @@
 ﻿namespace despesas_backend_api_net_core.Domain.VM
 {
     public class LoginVM
-    { 
+    {   
         public virtual int? IdUsuario { get; set; }
         public string? Email { get; set; }
         public string Senha { get; set; }
