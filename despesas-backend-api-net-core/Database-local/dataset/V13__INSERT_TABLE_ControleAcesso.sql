cb9a5067d58d1a7847c2e411b72868c4b0f63d40
Ribeiro <alexfariakof@gmail.com>
2023-11-30 17:19:04 -0300

    Update V13__INSERT_TABLE_ControleAcesso.sql

--- despesas-backend-api-net-core/Database-local/dataset/V13__INSERT_TABLE_ControleAcesso.sql
+++ despesas-backend-api-net-core/Database-local/dataset/V13__INSERT_TABLE_ControleAcesso.sql
@@ -1,2 +1,35 @@
 USE `DespesasPessoaisDB`;
-INSERT INTO `ControleAcesso` (`Id`, `Login`, `Senha`, `UsuarioId`) VALUES (1,'alexfariakof@gmail.com','G5BtHrnqJJGlg/QRi2hnOmt3EeLMB4M/38fy2lIG4mg=',1),(2,'teste@teste.com','afKN1iDmOHu39DlY8KmJmoXQrWYsCB4j4ZxLyxQ7BZE=',2),(3,'dns@dns.com','XA5hSF5rgAqwnIft5SM9ZH1VcOmjTe6zgXjiVn71WFI=',3),(4,'joao.silva5@gmail.com','w5qZPFcbbunIKqHaEzeeOPFZUNp5AXBYkfavRvpu3Vw=',4),(5,'lequinho.mumu@gmail.com','O8p40Qsi5UVuVO9WctvW6IEcsUwIBQ0foaqqa8EYxQQ=',5);
+
+INSERT INTO
+  `ControleAcesso` (`Id`, `Login`, `Senha`, `UsuarioId`)
+VALUES
+  (
+    1,
+    'alexfariakof@gmail.com',
+    'G5BtHrnqJJGlg/QRi2hnOmt3EeLMB4M/38fy2lIG4mg=',
+    1
+  ),
+  (
+    2,
+    'teste@teste.com',
+    '5bgAMsdJdv08DMKj4Eco8/COtUdfxTfrQjhU1jftVy4==',
+    2
+  ),
+  (
+    3,
+    'dns@dns.com',
+    'XA5hSF5rgAqwnIft5SM9ZH1VcOmjTe6zgXjiVn71WFI=',
+    3
+  ),
+  (
+    4,
+    'joao.silva5@gmail.com',
+    'w5qZPFcbbunIKqHaEzeeOPFZUNp5AXBYkfavRvpu3Vw=',
+    4
+  ),
+  (
+    5,
+    'lequinho.mumu@gmail.com',
+    '5bgAMsdJdv08DMKj4Eco8/COtUdfxTfrQjhU1jftVy4==',
+    5
+  );
