1ca3c6e001e7154919e21d4e234cde843aab04bb
Ribeiro <alexfariakof@gmail.com>
2023-12-06 14:39:54 -0300

    Update DataSeederImagemPerfilUsuario.cs

--- despesas-backend-api-net-core/Database-In-Memory/Implementations/DataSeederImagemPerfilUsuario.cs
+++ despesas-backend-api-net-core/Database-In-Memory/Implementations/DataSeederImagemPerfilUsuario.cs
@@ -6,34 +6,17 @@ namespace despesas_backend_api_net_core.Database_In_Memory.Implementations
     public class DataSeederImagemPerfilUsuario : IDataSeeder
     {
         private readonly RegisterContext _context;
+
         public DataSeederImagemPerfilUsuario(RegisterContext context)
         {
             _context = context;
         }
+
         public void SeedData()
         {
             if (!_context.ImagemPerfilUsuario.Any())
             {
-                var imagens = new List<ImagemPerfilUsuario>
-                {
-                    new ImagemPerfilUsuario
-                    {
-                        Id = 1,
-                        Url ="https://bucket-usuario-perfil.s3.amazonaws.com/1-imagem-perfil-usuario",
-                        Name = "1-imagem-perfil-usuario",
-                        Type = "jpg",
-                        UsuarioId = 1
-                    },
-                    new ImagemPerfilUsuario
-                    {
-                        Id = 2,
-                        Url =
-                            "https://bucket-usuario-perfil.s3.sa-east-1.amazonaws.com/2-imagem-perfil-usuario",
-                        Name = "2-imagem-perfil-usuario",
-                        Type = "jpg",
-                        UsuarioId = 2
-                    },
-                };
+                var imagens = new List<ImagemPerfilUsuario> { };
                 _context.ImagemPerfilUsuario.AddRange(imagens);
                 _context.SaveChanges();
             }
