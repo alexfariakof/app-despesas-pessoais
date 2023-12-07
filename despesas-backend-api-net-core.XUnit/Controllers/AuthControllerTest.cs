0910ab9bc4b66fb1e4e9ebca6d77324eb8b4741a
Ribeiro <alexfariakof@gmail.com>
2023-12-07 13:48:08 -0300

    Finish Implementations Unit Test AuthController

---
+++ despesas-backend-api-net-core.XUnit/Controllers/AuthControllerTest.cs
@@ -0,0 +1,59 @@
+﻿using despesas_backend_api_net_core.Controllers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
+using System.Security.Claims;
+
+namespace Test.XUnit.Controllers
+{
+    public class AuthControllerTest
+    {
+        protected readonly AuthController _authController;
+        private void SetupBearerToken(int userId)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, userId.ToString())
+            };
+            var identity = new ClaimsIdentity(claims, "IdUsuario");
+            var claimsPrincipal = new ClaimsPrincipal(identity);
+
+
+            var httpContext = new DefaultHttpContext
+            {
+                User = claimsPrincipal
+            };
+            httpContext.Request.Headers["Authorization"] = "Bearer " + Usings.GenerateJwtToken(userId);
+
+            _authController.ControllerContext = new ControllerContext
+            {
+                HttpContext = httpContext
+            };
+        }
+
+        public AuthControllerTest()
+        {            
+            _authController = new AuthController();
+            
+        }
+
+        public void IdUsuario_ShouldReturnCorrectUserId()
+        {
+            // Arrange
+            const int mockIdUsuario = 22;
+            SetupBearerToken(mockIdUsuario);
+
+            // Act
+            var result = GetProtectedProperty<int>(_authController, "IdUsuario");
+
+            // Assert
+            Assert.Equal(mockIdUsuario, result);
+        }
+
+        private T GetProtectedProperty<T>(object obj, string propertyName)
+        {
+            var propertyInfo = obj.GetType().GetProperty(propertyName, BindingFlags.Instance | BindingFlags.NonPublic);
+            return (T)propertyInfo.GetValue(obj);
+        }
+    }
+}
