272479603e2dbcf79cee4d6e84b0e50119486c93
Alex Ribeiro de Faria <42475620+alexfariakof@users.noreply.github.com>
2024-04-20 03:17:57 -0300

    Finish Implementations to fix bug in ImagemPerfilUsuario (#137)

--- MySqlServer.Migrations/Migrations/20240420061510_Update-ImagemPerfilUsuario.Designer.cs
+++ MySqlServer.Migrations/Migrations/20240420061510_Update-ImagemPerfilUsuario.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Repository;
 
@@ -10,9 +11,11 @@ using Repository;
 namespace MsMySqlServer.Migrations.Migrations
 {
     [DbContext(typeof(RegisterContext))]
-    partial class RegisterContextModelSnapshot : ModelSnapshot
+    [Migration("20240420061510_Update-ImagemPerfilUsuario")]
+    partial class UpdateImagemPerfilUsuario
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
