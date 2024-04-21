743b747e4e46168d6bf158378f1f88e5ae2984bd
Ribeiro <alexfariakof@gmail.com>
2024-04-21 04:04:38 -0300

    Refactory Categoria Controller

--- despesas-backend-api-net-core/Controllers/CategoriaController.cs
+++ despesas-backend-api-net-core/Controllers/CategoriaController.cs
@@ -13,11 +13,13 @@ public class CategoriaController : AuthController
     private readonly BusinessBase<CategoriaDto, Categoria> _categoriaBusiness;
     public CategoriaController(BusinessBase<CategoriaDto, Categoria> categoriaBusiness)
     {
-        _categoriaBusiness = categoriaBusiness;        
+        _categoriaBusiness = categoriaBusiness;
     }
 
     [HttpGet]
     [Authorize("Bearer")]
+    [ProducesResponseType((200), Type = typeof(List<CategoriaDto>))]
+    [ProducesResponseType((401), Type = typeof(UnauthorizedResult))]
     public IActionResult Get()
     {
         IList<CategoriaDto> _categoria = _categoriaBusiness.FindAll(IdUsuario).Result;
@@ -26,6 +28,8 @@ public class CategoriaController : AuthController
 
     [HttpGet("GetById/{idCategoria}")]
     [Authorize("Bearer")]
+    [ProducesResponseType((200), Type = typeof(CategoriaDto))]
+    [ProducesResponseType((401), Type = typeof(UnauthorizedResult))]
     public IActionResult GetById([FromRoute] int idCategoria)
     {
 
@@ -35,73 +39,82 @@ public class CategoriaController : AuthController
 
     [HttpGet("GetByTipoCategoria/{tipoCategoria}")]
     [Authorize("Bearer")]
+    [ProducesResponseType((200), Type = typeof(List<CategoriaDto>))]
+    [ProducesResponseType(typeof(UnauthorizedResult), StatusCodes.Status401Unauthorized)]
     public IActionResult GetByTipoCategoria([FromRoute] TipoCategoria tipoCategoria)
     {
         if (tipoCategoria == TipoCategoria.Todas)
         {
-            var _categoria = _categoriaBusiness.FindAll(IdUsuario).Result
-                             .Where(prop => prop.IdUsuario.Equals(IdUsuario)).ToList();
+            var _categoria = _categoriaBusiness.FindAll(IdUsuario).Result.Where(prop => prop.IdUsuario.Equals(IdUsuario)).ToList();
             return Ok(_categoria);
         }
         else
         {
-            var _categoria = _categoriaBusiness.FindAll(IdUsuario).Result
-                            .Where(prop => prop.IdTipoCategoria.Equals(((int)tipoCategoria))).ToList();
+            var _categoria = _categoriaBusiness.FindAll(IdUsuario).Result.Where(prop => prop.IdTipoCategoria.Equals(((int)tipoCategoria))).ToList();
             return Ok(_categoria);
-        }           
+        }
     }
 
     [HttpPost]
     [Authorize("Bearer")]
+    [ProducesResponseType(typeof(CategoriaDto), StatusCodes.Status200OK)]
+    [ProducesResponseType((400), Type = typeof(string))]
+    [ProducesResponseType(typeof(UnauthorizedResult), StatusCodes.Status401Unauthorized)]
     public IActionResult Post([FromBody] CategoriaDto categoria)
     {
 
         if (categoria.IdTipoCategoria == (int)TipoCategoria.Todas)
-            return BadRequest(new { message = "Nenhum tipo de Categoria foi selecionado!"});
+            return BadRequest("Nenhum tipo de Categoria foi selecionado!");
 
         try
         {
             categoria.IdUsuario = IdUsuario;
-            return Ok(new { message = true, categoria = _categoriaBusiness.Create(categoria) });
+            return Ok(_categoriaBusiness.Create(categoria));
         }
         catch
         {
-            return BadRequest(new { message = "Não foi possível realizar o cadastro de uma nova categoria, tente mais tarde ou entre em contato com o suporte." });
+            return BadRequest("Não foi possível realizar o cadastro de uma nova categoria, tente mais tarde ou entre em contato com o suporte.");
         }
     }
 
     [HttpPut]
     [Authorize("Bearer")]
+    [ProducesResponseType((200), Type = typeof(CategoriaDto))]
+    [ProducesResponseType((400), Type = typeof(string))]
+    [ProducesResponseType((401), Type = typeof(UnauthorizedResult))]
     public IActionResult Put([FromBody] CategoriaDto categoria)
     {
 
         if (categoria.TipoCategoria == TipoCategoria.Todas)
-            return BadRequest(new { message = "Nenhum tipo de Categoria foi selecionado!" });
+            return BadRequest("Nenhum tipo de Categoria foi selecionado!");
 
         categoria.IdUsuario = IdUsuario;
         CategoriaDto updateCategoria = _categoriaBusiness.Update(categoria);
 
         if (updateCategoria == null)
-            return BadRequest(new { message = "Erro ao atualizar categoria!" });
+            return BadRequest("Erro ao atualizar categoria!");
 
-        return Ok(new { message = true, categoria = updateCategoria });
+        return Ok(updateCategoria);
     }
 
     [HttpDelete("{idCategoria}")]
     [Authorize("Bearer")]
+    [ProducesResponseType((200), Type = typeof(bool))]
+    [ProducesResponseType((400), Type = typeof(string))]
+    [ProducesResponseType((401), Type = typeof(UnauthorizedResult))]
     public IActionResult Delete(int idCategoria)
     {
         CategoriaDto categoria = _categoriaBusiness.FindById(idCategoria, IdUsuario);
         if (categoria == null || IdUsuario != categoria.IdUsuario)
         {
-            return BadRequest(new { message = "Usuário não permitido a realizar operação!" });
+            return BadRequest("Usuário não permitido a realizar operação!");
         }
 
         if (_categoriaBusiness.Delete(categoria))
         {
-            return Ok(new { message = true });
+            return Ok(true);
         }
 
-        return new BadRequestObjectResult(new { message = false });
+        return new OkObjectResult(false);
     }        
 }
