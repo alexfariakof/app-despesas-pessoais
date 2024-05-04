1c471b82fe95bbfd42b373a38a03da09544134fb
Ribeiro <alexfariakof@gmail.com>
2024-05-04 05:18:13 -0300

    Update SwaggerApiVersioningDependenceInject.cs

--- despesas-backend-api-net-core/CommonDependenceInject/SwaggerApiVersioningDependenceInject.cs
+++ despesas-backend-api-net-core/CommonDependenceInject/SwaggerApiVersioningDependenceInject.cs
@@ -4,19 +4,19 @@ using Swashbuckle.AspNetCore.SwaggerGen;
 namespace despesas_backend_api_net_core.CommonDependenceInject;
 public static class SwaggerApiVersioningDependenceInject
 {
-    private readonly static string appName = "API Version with DDD/UnitOfWork/CQRS/HATEOAS";
-    private readonly static string appVersion = "v1";
+    private readonly static string appName = "API Restful with DDD/UnitOfWork/CQRS/HATEOAS";
+    private readonly static string currenetVersion = "v2";
     private readonly static string appDescription = $"API Restful HATEOAS .Net Core 8.0 com CI/CD, Dockerizada utilizando Entity Framework  e Migrations.";
 
     public static void AddSwaggerApiVersioning(this IServiceCollection services)
     {
         services.AddSwaggerGen(c =>
         {
-            c.SwaggerDoc(appVersion,
+            c.SwaggerDoc("v1",
             new OpenApiInfo
             {
                 Title = appName,
-                Version = appVersion,
+                Version = "v1",
                 Description = appDescription,
                 Contact = new OpenApiContact
                 {
@@ -32,11 +32,11 @@ public static class SwaggerApiVersioningDependenceInject
                 return controllerNamespace != null && controllerNamespace.Contains("Controllers.v1");
             });
 
-            c.SwaggerDoc("v2",
+            c.SwaggerDoc(currenetVersion,
                 new OpenApiInfo
                 {
                     Title = appName,
-                    Version = "v2",
+                    Version = currenetVersion,
                     Description = appDescription,
                     Contact = new OpenApiContact
                     {
@@ -46,12 +46,11 @@ public static class SwaggerApiVersioningDependenceInject
                 });
 
 
-            // Inclui apenas os controllers do namespace 'Controllers.v2' na documentação da versão 2
             c.DocInclusionPredicate((docName, apiDesc) =>
             {
                 if (!apiDesc.TryGetMethodInfo(out var methodInfo)) return false;
                 var controllerNamespace = methodInfo.DeclaringType.Namespace;
-                return controllerNamespace != null && controllerNamespace.Contains("Controllers.v2");
+                return controllerNamespace != null && controllerNamespace.Contains($"Controllers.{currenetVersion}");
             });
         });
     }
@@ -62,8 +61,8 @@ public static class SwaggerApiVersioningDependenceInject
         app.UseSwaggerUI(c =>
         {
             string swaggerJsonBasePath = string.IsNullOrWhiteSpace(c.RoutePrefix) ? "." : "..";
-            c.SwaggerEndpoint($"{swaggerJsonBasePath}/swagger/{appVersion}/swagger.json", $"{appVersion} {appName} ");
-            c.SwaggerEndpoint($"{swaggerJsonBasePath}/swagger/v2/swagger.json", $"v2 {appName}");
+            c.SwaggerEndpoint($"{swaggerJsonBasePath}/swagger/v1/swagger.json", $"v1 {appName}");
+            c.SwaggerEndpoint($"{swaggerJsonBasePath}/swagger/{currenetVersion}/swagger.json", $"{currenetVersion} {appName} ");            
         });
     }
 }
