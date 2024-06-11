bc967f0ec390415fc286fab9b6586343635f2a4e
Alex Ribeiro de Faria <42475620+alexfariakof@users.noreply.github.com>
2024-06-11 15:54:12 -0300

    Update SwaggerApiVersioningDependenceInject.cs

--- Despesas.WebApi/CommonDependenceInject/SwaggerApiVersioningDependenceInject.cs
+++ Despesas.WebApi/CommonDependenceInject/SwaggerApiVersioningDependenceInject.cs
@@ -5,14 +5,9 @@ using System.Reflection;
 namespace Despesas.WebApi.CommonDependenceInject;
 public static class SwaggerApiVersioningDependenceInject
 {
-    private readonly static string appName = "API Despesas Pessoais RESTful com HATEOAS ";
+    private readonly static string appName = "API Despesas Pessoais RESTful";
     private readonly static string currentVersion = "v2";
-    private readonly static string appDescription = @$"
-       Este serviço é uma Web API RESTful com suporte a HATEOAS, desenvolvida utilizando .NET Core. O serviço é totalmente dockerizado e utiliza Entity Framework com Migrations para gerenciar o banco de dados.
-       Implementamos um pipeline de CI/CD para garantir a integração contínua e a entrega contínua, proporcionando um desenvolvimento ágil e eficiente. O serviço está implantado em ambientes de desenvolvimento (DEV) e produção (PROD), hospedados em instâncias EC2 da AWS.
-       Além disso, temos build,testes unitários e teste de cobertura de código estático ""SONAR CLOUD"" rodando estrategicamente a cada push ou pull request realizadas em branchs do git hub. 
-       Também contamos com um serviço de testes de ponta a ponta (E2E) desenvolvido com Python e Playwright. Esse serviço de testes é executado automaticamente através de triggers configuradas no GitHub Actions, assegurando que a qualidade e a funcionalidade do aplicativo sejam verificadas em cada alteração de código.";
-
+    private readonly static string appDescription = @$"";
     public static void AddSwaggerApiVersioning(this IServiceCollection services)
     {
         services.AddSwaggerGen(c =>
@@ -30,7 +25,7 @@ public static class SwaggerApiVersioningDependenceInject
             });
 
             c.SwaggerDoc(currentVersion, new OpenApiInfo                {
-                Title = appName,
+                Title = $" { appName} com HATEOAS",
                 Version = currentVersion,
                 Description = appDescription,
                 Contact = new OpenApiContact                    {
