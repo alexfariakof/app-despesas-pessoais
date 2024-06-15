14ef5f869801ef3f548b497412d21a93d120ae9b
Alex Ribeiro de Faria <42475620+alexfariakof@users.noreply.github.com>
2024-06-15 12:55:48 -0300

    Update coverage_report.ps1

--- XunitTests/coverage_report.ps1
+++ XunitTests/coverage_report.ps1
@@ -7,16 +7,27 @@ $reportPath = Join-Path -Path (Get-Location) -ChildPath "TestResults"
 $coveragePath = Join-Path -Path $reportPath -ChildPath "coveragereport"
 $coverageAngularPath = Join-Path -Path $projectAngular -ChildPath "coverage"
 
+function Wait-Angular-TestResults {
+    $REPEAT_WHILE = 0
+    while (-not (Test-Path $coverageAngularPath)) {
+        echo "Agaurdando Coverage Report..."
+        Start-Sleep -Seconds 10        
+        if ($REPEAT_WHILE -eq 6) { break }
+        $REPEAT_WHILE = $REPEAT_WHILE + 1
+    }
+} 
+
 # Gera o Relatório de Cobertura do Backend
 dotnet test ./XUnit.Tests.csproj --configuration Staging --results-directory $reportPath /p:CollectCoverage=true /p:CoverletOutputFormat=cobertura --collect:"XPlat Code Coverage;Format=opencover" --no-restore > $null 2>&1
 reportgenerator -reports:$projectTestPath\coverage.cobertura.xml  -targetdir:$coveragePath -reporttypes:"Html;lcov;" -sourcedirs:$sourceDirs -filefilters:-$filefilters > $null 2>&1
 
 # Verifica se existe a pasta node_module, e sem não existir executa npm install 
 if (-not (Test-Path $projectAngular\node_modules)) {
-    $watchProcess = Start-Process npm -ArgumentList "install" -WorkingDirectory $projectAngular -NoNewWindow -PassThru
-    $watchProcess.WaitForExit()	
+	cd $projectAngular
+	npm install
+	cd $projectTestPath 
 }
 
 # Executa Teste Unitários e gera o relatório de cobertura do Frontend 
-$watchProcess = Start-Process npm -ArgumentList "run", "test:coverage" -WorkingDirectory $projectAngular -NoNewWindow -PassThru
-$watchProcess.WaitForExit()	
+Start-Process npm -ArgumentList "run", "test:coverage" -WorkingDirectory $projectAngular -NoNewWindow
+Wait-Angular-TestResults
