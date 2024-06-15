de8a0359a43da0fde4f6499e9a7fd69f71874ab2
Alex Ribeiro de Faria <42475620+alexfariakof@users.noreply.github.com>
2024-06-15 07:46:04 -0300

    Optimazing-Test-and-Analysis-Code-In-Sonar-Cloud

--- XunitTests/coverage_report.ps1
+++ XunitTests/coverage_report.ps1
@@ -8,7 +8,7 @@ $coveragePath = Join-Path -Path $reportPath -ChildPath "coveragereport"
 $coverageAngularPath = Join-Path -Path $projectAngular -ChildPath "coverage"
 
 # Gera o Relatório de Cobertura do Backend
-dotnet test ./XUnit.Tests.csproj --configuration Release --results-directory $reportPath /p:CollectCoverage=true /p:CoverletOutputFormat=cobertura --collect:"XPlat Code Coverage;Format=opencover" --no-restore > $null 2>&1
+dotnet test ./XUnit.Tests.csproj --configuration Staging --results-directory $reportPath /p:CollectCoverage=true /p:CoverletOutputFormat=cobertura --collect:"XPlat Code Coverage;Format=opencover" --no-restore > $null 2>&1
 reportgenerator -reports:$projectTestPath\coverage.cobertura.xml  -targetdir:$coveragePath -reporttypes:"Html;lcov;" -sourcedirs:$sourceDirs > $null 2>&1
 
 # Verifica se existe a pasta node_module, e sem não existir executa npm install 
