c059c24d82635d8fcc25bc0eb745be4f2c705be4
Alex Ribeiro de Faria <42475620+alexfariakof@users.noreply.github.com>
2024-06-16 17:32:28 -0300

    bugfix/Test-and-Analysis-Code-In-Sonar-Cloud

--- XunitTests/coverage_report.ps1
+++ XunitTests/coverage_report.ps1
@@ -5,13 +5,12 @@ $sourceDirs = "$baseDirectory\Despesas.Business;$baseDirectory\Despesas.Domain;$
 $filefilters = "$baseDirectory\Despesas.DataSeeders\**;-$baseDirectory\Migrations.MySqlServer\**;-$baseDirectory\Migrations.MsSqlServer\**;-$baseDirectory\Despesas.CrossCutting\**;-$baseDirectory\Despesas.Business\HyperMedia\**"
 $reportPath = Join-Path -Path (Get-Location) -ChildPath "TestResults"
 $coveragePath = Join-Path -Path $reportPath -ChildPath "coveragereport"
-$#coverageAngularPath = Join-Path -Path $projectAngular -ChildPath "coverage"
+#$coverageAngularPath = Join-Path -Path $projectAngular -ChildPath "coverage"
 
 # Gera o Relatório de Cobertura do Backend
 dotnet test ./XUnit.Tests.csproj --configuration Staging --results-directory $reportPath /p:CollectCoverage=true /p:CoverletOutputFormat=cobertura --collect:"XPlat Code Coverage;Format=opencover" --no-restore > $null 2>&1
 reportgenerator -reports:$projectTestPath\coverage.cobertura.xml  -targetdir:$coveragePath -reporttypes:"Html;lcov;" -sourcedirs:$sourceDirs -filefilters:-$filefilters
 
-Exit
 # Verifica se existe a pasta node_module, e sem não existir executa npm install 
 #if (-not (Test-Path $projectAngular\node_modules)) {
 #    $watchProcess = Start-Process npm -ArgumentList "install" -WorkingDirectory $projectAngular -NoNewWindow -PassThru
