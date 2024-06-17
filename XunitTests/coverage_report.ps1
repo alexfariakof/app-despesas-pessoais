d4081a9596a3023e9c52e6db07e9d1cae42427b5
Alex Ribeiro de Faria <42475620+alexfariakof@users.noreply.github.com>
2024-06-17 18:04:43 -0300

    bugfix/Testing build before execute test to coverage get report files

--- XunitTests/coverage_report.ps1
+++ XunitTests/coverage_report.ps1
@@ -30,6 +30,12 @@ if (-not (Test-Path $projectAngular\node_modules)) {
     $watchProcess.WaitForExit()	
 }
 
+# Verifica se existe a pasta dist, e sem não existir executa npm build 
+if (-not (Test-Path $projectAngular\dist)) {
+    $watchProcess = Start-Process npm -ArgumentList "build" -WorkingDirectory $projectAngular -NoNewWindow -PassThru
+    $watchProcess.WaitForExit()	
+}
+
 # Executa Teste Unitários e gera o relatório de cobertura do Frontend 
 $watchProcess = Start-Process npm -ArgumentList "run", "test:coverage" -WorkingDirectory $projectAngular -NoNewWindow -PassThru
 $watchProcess.WaitForExit()	
