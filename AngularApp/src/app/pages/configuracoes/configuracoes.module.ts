93750901473032935541861f1ff9532d21f981c7
Alex Ribeiro de Faria <42475620+alexfariakof@users.noreply.github.com>
2024-06-22 11:52:39 -0300

    Implmentations to fix bug

--- AngularApp/src/app/pages/configuracoes/configuracoes.module.ts
+++ AngularApp/src/app/pages/configuracoes/configuracoes.module.ts
@@ -6,8 +6,11 @@ import { ChangeAvatarComponent } from './change-avatar/change-avatar.component';
 import { ChangePasswordComponent } from './change-password/change-password.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { SharedModule } from '../../shared/shared.module';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 @NgModule({
   declarations : [ConfiguracoesComponent, ChangeAvatarComponent, ChangePasswordComponent],
-  imports: [CommonModule, ReactiveFormsModule, ConfiguracoesRoutingModule, SharedModule]
+  imports: [CommonModule, ReactiveFormsModule, ConfiguracoesRoutingModule, MatIconModule, MatFormFieldModule, MatInputModule, SharedModule]
 })
 export class ConfiguracoesModule {}
